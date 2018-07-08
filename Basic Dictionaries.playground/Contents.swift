51315d2216b70f8f93f52124e3762babccee6c58
jolson615 <jolson615@gmail.com>
2018-07-08 18:51:08 -0400

    initial commit

---
+++ Basic Dictionaries.playground/Contents.swift
@@ -0,0 +1,74 @@
+//: Playground - noun: a place where people can play
+
+// Here's an example dictionary that shows everyone's lucky numbers.
+var luckyNumbers: [String: Int] = [
+    "Jeff" : 42,
+    "Taylor" : 7,
+    "Cristian" : 99,
+    "Alexandra" : 13
+]
+
+// Level 1:
+// Print out Cristian's favorite number.
+
+
+
+// Try print out a number that isn't there, like Danny's.
+
+
+
+// Try unwrapping the optional statement by adding in the nil coalescing operator to add a fallback value.
+// For example, print(luckyNumbers["Jeff"] ?? 0) will print out a zero if it can't locate Jeff's favorite number in the dictionary.
+
+
+
+// Level 2:
+// Create a dictionary of how many grams of protein are in certain snacks. The first two entries have been created for you:
+var proteinIn: [String: Int] = [
+    "Ice cream" : 4,
+    "Egg" : 6
+]
+
+
+// Create another dictionary that logs some other nutritional information about those same foods.
+
+
+
+// Try printing out a full sentence about one of the foods based on the infromation stored in both dictionaries.
+
+
+
+// Level 3:
+// Create a dictionary of strings paired to strings that shows everyone at your table's favorite movie.
+
+
+
+// Print out two values that are found in that dictionary and try printing one that isn't found in that dictionary.
+
+
+
+// Create two similar dictionaries; one with favorite books and one with favorite musical artists/bands.
+
+
+
+// Add another person (your TA or teacher) to each of those dictionaries without modifying the original dictionary declaration. Then try printing out some of the dictionaries to confirm that it worked.
+
+
+
+// Level 4:
+// Iterate over the movies dictionary and print out "____ really loves the movie ____" for each of the key-value pairs.
+
+
+
+
+// Do the same thing for the books and music dictionaries.
+
+
+
+
+// Level 4:
+// Create a function called printFavorites that takes a dictionary like the ones we've made and prints out "____ really loves ______".
+
+
+
+// Call that function on all three of our dictionaries of favorites. 
